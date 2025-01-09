bdc3d0f13834431d0b26b4096bbe4776f23e0450
boatconsider <boatzarock01@gmai.com>
2025-01-09 15:24:15 +0700

    ddd

--- src/receive-message.js
+++ src/receive-message.js
@@ -15,6 +15,6 @@ export default function handler(req, res) {
       res.status(200).json({ success: true, message: 'Message received successfully' });
   } else {
       // ถ้าไม่ใช่ POST ให้ส่ง error 405
-      res.status(405).json({ error: 'Method Not Allowed' });
+      res.status(400).json({ error: 'Method Not Allowed' });
   }
 }
